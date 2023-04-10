0776d5a1cb9fe2b92441d7076de70f2b6548375a
Kyuhan1230 <asdm159@naver.com>
2023-04-10 16:43:23 +0900

    Feat: Numpy 타입 -> Python 타입(Float) 변환 기능 추가

---
+++ classify_fault/utils/convert_type.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def numpy_to_python_type(value):
+    if isinstance(value, (np.int32, np.int64)):
+        return int(value)
+    elif isinstance(value, (np.float32, np.float64)):
+        return float(value)
+    elif isinstance(value, (float, int)):
+        return value
+    else:
+        raise TypeError(f"Unsupported data type: {type(value)}")
